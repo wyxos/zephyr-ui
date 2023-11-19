d22f197ace061b4e6f1c2b2f7f9cecc3b27369f2
Joey J <joey.jaulimsing@live.com>
2023-11-19 11:42:55 +0400

    fix: refactor

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'workspace', 'middleware' => 'web'], function () {
     Route::get('/{page?}', function () {
-        $configPath = base_path('vendor/wyxos/zephyr-ui/dist/dev-config.json');
+        $configPath = base_path('../../../wyxos/js/zephyr-spa/dist/dev-config.json');
         $devServerConfig = file_exists($configPath) ? json_decode(file_get_contents($configPath), true) : null;
 
         return view('taskmanager::dashboard')->with([
